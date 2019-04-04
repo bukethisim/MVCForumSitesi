e3f80ffe5e9e5793cee47749618dce99e6a39c67
Buket hısım <bukethisim07@hotmail.com>
2019-04-04 12:32:05 +0300

    Home Controller Düzenleme

--- MVCForumSitesi/MVCForumSitesi/Controllers/HomeController.cs
+++ MVCForumSitesi/MVCForumSitesi/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using BLL;
+using Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +15,12 @@ namespace MVCForumSitesi.Controllers
         UnitOfWork _uw = new UnitOfWork();
         public ActionResult Index()
         {
+            UserStore<Person> store = new UserStore<Person>(UnitOfWork.Create());
+            UserManager<Person> manager = new UserManager<Person>(store);
+            ViewBag.Category = _uw.Categories.GetAll().Count();
+            ViewBag.Student = manager.Users.Where(x => x.hasLesson == false).Count();
+            ViewBag.Teacher = manager.Users.Where(x => x.hasLesson == true).Count();
+            ViewBag.TotalQ = _uw.Questions.GetAll().Count();
             return View();
         }
 
